92724bd63b704390249a4fc94be10c614b39045b
curious-abdulghaffar <abdulghaffar4324@gmail.com>
2026-08-31 05:45:04 +0500

    route modified

--- server/server.js
+++ server/server.js
@@ -25,6 +25,7 @@ const allowedOrigins = [
     'http://localhost:3000'
   ].filter(Boolean);
   
+  app.use('/api/auth', authRoutes);git 
   app.use(cors({
     origin: (origin, callback) => {
       // Allow requests with no origin (like mobile apps or curl requests)
@@ -53,7 +54,7 @@ app.use('/api/complaints', complaintRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
-app.use('/api/auth', authRoutes);
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, '0.0.0.0', () => {
